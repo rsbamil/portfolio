73379834ffb91ce1385098c9df8bbb22d235d12c
Rohit Kumar <rsbamil66@gamil.com>
2025-07-10 10:40:57 +0530

    adding animations

--- src/components/BagEffectName.jsx
+++ src/components/BagEffectName.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
-
+import { motion } from "framer-motion";
 const NameBagEffect = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleBag = () => setIsOpen(!isOpen);
 
   return (
-    <div className="w-[40%]  flex h-[500px] bg-transparent  bg-opacity-75">
+    <motion.div
+      initial={{ opacity: 0, x: 200 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 20,
+        damping: 10,
+        duration: 1.5,
+        delay: 2,
+      }}
+      className="w-[40%]  flex h-[500px] bg-transparent  bg-opacity-75"
+    >
       {/* Right Side */}
       <div className="flex items-center justify-center relative overflow-hidden">
         <div className="text-center">
@@ -71,7 +82,7 @@ const NameBagEffect = () => {
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
